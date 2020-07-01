749a03d9a314ccd963150ea73ed52d6558ebee49
Adrien Ickowicz <adrien.ickowicz@csiro.au>
2020-07-01 10:59:37 +1000

    updating description and licensing

--- R/convReg-package.R
+++ R/convReg-package.R
@@ -1,11 +1,14 @@
-#' convReg : Package for convolutive mixture models.
+#' \pkg{convReg}
 #'
 #' convReg allows you to infer convolutive mixture models.
 #' @useDynLib convReg
+#' @name vesselett
+#' @docType package
 #' @importFrom Rcpp sourceCpp
 #' @importFrom grDevices hcl rgb
 #' @importFrom graphics abline arrows axis filled.contour grid hist layout legend par plot plot.new points polygon text
 #' @importFrom utils combn
 #' @importFrom COMPoissonReg glm.cmp rcmp dcmp
+#' @importFrom Rdpack reprompt
 #' @import RcppArmadillo compiler data.table doMC doParallel foreach inline lattice nnet numDeriv parallel pscl stats MASS
-"_PACKAGE"
+NULL
